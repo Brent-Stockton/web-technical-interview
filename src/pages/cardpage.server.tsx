5466a04698a00a44a5ac5e7a2b9f5299895345bd
brent stockton <brentstockton@brents-MacBook-Pro.local>
2023-05-13 18:33:37 -0700

    added getServerSide Props Function to prevent visiting /cardpage without logging in

---
+++ src/pages/cardpage.server.tsx
@@ -0,0 +1,28 @@
+import { IncomingMessage, ServerResponse } from "http"
+
+// function checks if user cookies exist on req object. If it doesn't, this will redirect the user to login page. Uses 302 for "temporary redirect"
+export default async function getServerSideProps({
+  req,
+  res,
+}: {
+  req: IncomingMessage
+  res: ServerResponse
+}) {
+  const userString =
+    req.headers.cookie
+      ?.split("; ")
+      .find((cookie) => cookie.startsWith("user="))
+      ?.split("=")[1] || ""
+  const user = userString ? JSON.parse(userString) : null
+
+  if (!user) {
+    res.setHeader("location", "/")
+    res.statusCode = 302
+    res.end()
+    return { props: {} }
+  }
+
+  return {
+    props: {},
+  }
+}
